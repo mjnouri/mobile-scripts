11c7015e5d0af6c457036eb27f458ce94a2a360a
Mark Nouri <mnouri@coreebusiness.com>
2020-12-09 14:56:35 -0500

    cleaned check-logs.sh

--- check-logs.sh
+++ check-logs.sh
@@ -6,5 +6,5 @@ DIR2WC=$(ls -l /opt/liferay/tomcat-7.0.42/bin/C\:/liferay_tomcat/liferay-portal-
 DIR1SIZE=$(du -sh /opt/liferay/tomcat-7.0.42/logs)
 DIR2SIZE=$(du -sh /opt/liferay/tomcat-7.0.42/bin/C\:/liferay_tomcat/liferay-portal-6.2.0-ce-ga1/logs)
 
-echo "Dir1 has $DIR1WC file(s) and is $DIR1SIZE." | tee -a log
-echo "Dir2 has $DIR2WC file(s) and is $DIR2SIZE." | tee -a log
+echo "Dir1 has $DIR1WC file(s) and is $DIR1SIZE."
+echo "Dir2 has $DIR2WC file(s) and is $DIR2SIZE."
